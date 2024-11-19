d0fd8259538d6a65c7e57f4341fe0e4ea3d122e7
sixtick_sub3 <business4dyd@gmail.com>
2024-11-19 15:09:39 +0900

    Initial commit

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,11 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) { // 부트 앱 진입점
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}

09b1a8e13db2cc1863460bcbc56bacfe783f478b
sixtick_sub3 <business4dyd@gmail.com>
2024-11-19 17:39:22 +0900

    120 페이지 중

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/QuizController.java
@@ -0,0 +1,29 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuizController {
+
+    @GetMapping("/quiz")
+    public ResponseEntity<String> quiz(@RequestParam("code") int code){
+        return switch (code) {
+            case 1 -> ResponseEntity.created(null).body("Created!");
+            case 2 -> ResponseEntity.badRequest().body("Bad Request!");
+            default -> ResponseEntity.ok().body("OK!");
+        };
+    }
+
+    public ResponseEntity<String> quiz2(@RequestBody Code code){
+        return switch (code.value()){
+            case 1 -> ResponseEntity.status(403).body("Forbidden!");
+            default -> ResponseEntity.ok().body("OK!");
+        };
+    }
+
+    record Code(int value){}
+}
---
+++ src/test/java/me/shinsunyoung/springbootdeveloper/TestControllerTest.java
@@ -0,0 +1,66 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest // SpringBootApplication 애노테이션이 있는 클래스를 찾아서 연관된 빈을 테스트용 컨텍스를 만듬
+@AutoConfigureMockMvc   // 테스트용 MVC 환경을 제공, 컨트롤러 테스트할때 사용됨
+class TestControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+    }
+
+    @AfterEach
+    public void cleanUp(){
+        memberRepository.deleteAll();
+    }
+
+    @DisplayName("getAllMembers: 아티클 조회에 성공한다.")
+    @Test
+    public void getAllMembers() throws Exception {
+        // given 맴버를 저장
+        final  String url = "/test";
+        Member savedMember = memberRepository.save(new Member(1L, "홍길동"));
+
+        // when 조회
+        final ResultActions result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON));
+
+        //then 검증
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
+                .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
+
+    }
+
+
+
+
+}
